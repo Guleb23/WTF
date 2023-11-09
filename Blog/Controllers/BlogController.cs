11b1faa33303ec98db05cb90bf299072f4dea501
unknown <tonovgleb@gmail.com>
2023-11-09 03:24:36 +0300

    first commit

---
+++ Blog/Controllers/BlogController.cs
@@ -0,0 +1,52 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Blog.Controllers
+{
+    public class BlogController : Controller
+    {
+        static List<Posts> Posts = new List<Posts>();
+
+
+        public IActionResult Index()
+        {
+            return View("Index", Posts);
+        }
+
+
+        public IActionResult CreatePost(Guid id)
+        {
+            if (id != Guid.Empty)
+            {
+                Posts existPost = Posts.FirstOrDefault(p => p.Id == id);
+                return View(model: existPost);
+            }
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CreatePost(Posts enry)
+        {
+            
+            if (enry.Id == Guid.Empty)
+            {
+                Posts newPost = new Posts();
+                newPost.Id = Guid.NewGuid();
+                newPost.Description = enry.Description;
+                newPost.Name = enry.Name;
+                newPost.DatePublic = DateTime.Now;
+                Posts.Add(newPost);
+            }
+            else
+            {
+                var exsPost = Posts.FirstOrDefault(post => post.Id == enry.Id);
+                exsPost.Name = enry.Name;
+                exsPost.Description = enry.Description;
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Blog/Models/Posts.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Models
+{
+    public class Posts
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public DateTime DatePublic { get; set; }
+    }
+}
